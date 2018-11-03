84f531bfe9ee2430f4ff652371dbc387c63a33b3
Santimondra <santiagomondram@gmail.com>
2018-11-03 18:43:52 -0500

    mini changes to singup style

--- Containers/SignUp.js
+++ Containers/SignUp.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
-
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button } from 'react-native-elements'
 export default class SignUp extends React.Component {
 
 render() {
@@ -8,20 +8,32 @@ render() {
       <View style={styles.container}>
         <Text>Sign Up</Text>
       
+        <Text
+          style={styles.textLabel}
+        >Correo Empresarial</Text>
         <TextInput
-          placeholder="Correo empresarial"
+          placeholder="pgarcia@vibo.com"
           autoCapitalize="none"
           style={styles.textInput}
         />
+        <Text
+          style={styles.textLabel}>Contraseña</Text>
         <TextInput
           secureTextEntry
-          placeholder="Contraseña"
+          placeholder="••••••••"
           autoCapitalize="none"
           style={styles.textInput}
         />
-        <Button title="Registrarse" />
+        <Button title="Registrarse"
+        titleStyle={{color: "white"}}
+        buttonStyle={styles.buttonStyle}
+        />
+        <Text
+          style={styles.textRegistrate}>Ya tienes una cuenta?</Text>
         <Button
-          title="Ya tienes una cuenta? Inicia sesión"
+          title="Inicia sesión"
+          titleStyle={styles.buttonTextRegistrate}
+          buttonStyle={styles.buttonRegistrate}
         />
       </View>
     )
@@ -32,13 +44,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: "#FBFBFB",
+    padding: 20
   },
   textInput: {
     height: 40,
-    width: '90%',
-    borderColor: 'gray',
+    width: '100%',
+    backgroundColor: 'white',
     borderWidth: 1,
-    marginTop: 8
+    marginBottom: 15,
+    padding: 10
+  },
+  textLabel: {
+    width: "100%",
+    textAlign: "left",
+    margin: 10,
+    color: "black"
+  },
+  buttonStyle: {
+    backgroundColor: "#310432",
+    width: "50%"
+  },
+  textRegistrate: {
+    marginTop: 20,
+    marginBottom: 10,
+    color: "#310432",
+  },
+  buttonRegistrate: {
+    backgroundColor: "#FBFBFB",
+  },
+  buttonTextRegistrate: {
+    color: "#310432",
+    fontSize: 14,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
   }
 })
